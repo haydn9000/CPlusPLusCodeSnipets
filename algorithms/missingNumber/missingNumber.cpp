6841d4fa3e77f6676428a9bcf83721843bad3b39
Haydn <haydnady@gmail.com>
2022-06-19 14:03:09 -0400

    Added tests

--- algorithms/missingNumber/missingNumber.cpp
+++ algorithms/missingNumber/missingNumber.cpp
@@ -9,9 +9,9 @@ int findMissingNumber(vector<int> &vec)
 
     sort(vec.begin(), vec.end());
 
-    for (int i = 0; i <= vec.size(); i++)
+    for (int i = 0; i < vec.size(); i++)
     {
-        if (vec.at(i + 1) != i + 1)
+        if (vec.at(i) != i + 1)
         {
             return i + 1;
         }
@@ -22,8 +22,20 @@ int findMissingNumber(vector<int> &vec)
 
 int main()
 {
-    vector<int> vec = {};
+    // Test 1
+    vector<int> vec = {10, 9, 6, 5, 4, 7 , 3, 2, 1};
 
-    cout << "The missing number is: " << findMissingNumber(vec) << endl;
+    cout << "\nThe missing number is (expected 8): " << findMissingNumber(vec) << endl;
+
+    // Test 2
+    vector<int> vec2 = {8, 10, 9, 6, 5, 4, 7, 13, 3, 2, 1, 12};
+
+    cout << "\nThe missing number is (expected 11): " << findMissingNumber(vec2) << endl;
+
+    // Test 3
+    vector<int> vec3 = {10, 9, 6, 5, 8, 7 , 3, 2, 1, 11};
+
+    cout << "\nThe missing number is (expected 4): " << findMissingNumber(vec3) << endl << endl;
+
     return 0;
 }
