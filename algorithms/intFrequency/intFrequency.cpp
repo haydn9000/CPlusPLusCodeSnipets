19fddddc041dfedfef6dc49b2edd8f66fa712aed
Haydn <haydnady@gmail.com>
2022-06-19 17:05:25 -0400

    WIP

---
+++ algorithms/intFrequency/intFrequency.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<vector<int>> freqCount(vector<int> &vec, int &number)
+{
+
+    return {{}};
+}
+
+int main()
+{
+
+    // Test 1
+    vector<int> vec = {1, 4, 4, {1, 1, {1, 2, 1, 1}}};
+    int number = 1;
+
+
+    return 0;
+}
+
+// WIP what data structures would make this work best 
+// (for nested array of arrays and numbers)?
